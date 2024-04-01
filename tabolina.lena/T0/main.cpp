7f5ae101656b2e589e5123fbd01571c5a2a25b67
tabolina.lena <tabolinalena0407@mail.ru>
2024-04-01 21:14:10 +0300

    Исправила табуляцию в коде

--- tabolina.lena/T0/main.cpp
+++ tabolina.lena/T0/main.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
-using namespace std;
-int main() {
-	cout << "tabolina.lena" << std::endl;
-	return 0;
+#include <iostream>
+
+int main()
+{
+  std::cout << "tabolina.lena\n";
+  return 0;
 }
