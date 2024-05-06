2930cbfcead1cf0e9ac16641217226ae2221b2e0
hismatulin.vadim <vadim-chita@yandex.ru>
2024-05-06 18:31:16 +0300

    убрал лишние пробелы

--- hismatulin.vadim/T2/namespace.h
+++ hismatulin.vadim/T2/namespace.h
@@ -1,56 +1,40 @@
+#pragma once
 #ifndef NAMESPACE_H
 #define NAMESPACE_H
 
 #include "local.h"
-
 namespace hismatulin
 {
-  // ULL LIT
-  // CMP LSP
-
   struct DataStruct
   {
     unsigned long long key1;
     std::complex<double> key2;
     std::string key3;
   };
-
   struct DelimiterIO
   {
     char exp;
   };
-
-  // COMPLEX&
   struct CMPDouble_ST
   {
     std::complex<double>& ref;
   };
-
-  // ULL&
   struct ULL_ST
   {
     unsigned long long& ref;
   };
-
-  // DOUBLE&
   struct Double_ST
   {
     double& num;
   };
-
-  // STRING&
   struct String_ST
   {
     std::string& ref;
   };
-
-  // STRING
   struct Label_ST
   {
     std::string exp;
   };
-
-  // scope guard 
   class iofmtguard
   {
   public:
@@ -62,7 +46,6 @@ namespace hismatulin
     std::streamsize precision_;
     std::basic_ios< char >::fmtflags fmt_;
   };
-
   std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
   std::istream& operator>>(std::istream& in, CMPDouble_ST&& dest);
   std::istream& operator>>(std::istream& in, ULL_ST&& dest);
@@ -73,5 +56,6 @@ namespace hismatulin
 
   bool compareDataStruct(const DataStruct& ds_first, const DataStruct& ds_second);
 }
-
 #endif
+
+
