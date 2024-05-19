158dd0a83d2f96c21d98df6867eec112993baddf
Quasar0707 <marsokolov010@gmail.com>
2024-05-20 02:32:56 +0300

    add more files and write structure of main

---
+++ sokolov.mark/T3/Shape.h
@@ -0,0 +1,6 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+
+
+#endif
--- sokolov.mark/T3/main.cpp
+++ sokolov.mark/T3/main.cpp
@@ -1,7 +1,64 @@
-#include "iostream"
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include "Shape.h"
+#include "Commands.h"
 
-int main()
+//ERRORS
+
+int main(int argc,char* argv[])
 {
+  if (argc != 2)
+  {
+	//error
+	return EXIT_FAILURE;
+  }
 
-  return 0;
+  //std::vector<*shape*> shapes;
+
+  std::string filename = argv[1];
+  std::ifstream fin(filename); //open file
+
+  if (!fin.is_open())
+  {
+	//error
+	return EXIT_FAILURE;
+  }
+
+  while (!fin.eof())
+  {
+	//считывание фигур
+	//запись в вектор
+  }
+
+  std::cout << std::fixed;
+  std::cout << std::setprecision(1);
+
+  try
+  {
+	while (!std::cin.eof())
+	{
+	  std::string command;
+	  std::cin >> command;
+
+	  try
+	  {
+		//executing commands
+	  }
+	  catch (const std::string& err)
+	  {
+		std::cerr << err << std::endl;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	  }
+	}
+
+	return EXIT_SUCCESS;
+  }
+  catch (...)
+  {
+	std::cerr << "ERROR!" << std::endl;
+	return EXIT_FAILURE;
+  }
 }
