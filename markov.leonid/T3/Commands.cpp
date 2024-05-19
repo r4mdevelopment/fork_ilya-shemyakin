95863bdfa2d7d286f0dfae75d0f9785275370fff
mrkv 4449 <markoffleonid@mail.ru>
2024-05-19 18:41:16 +0300

    fixing indentations[2]

--- markov.leonid/T3/Commands.cpp
+++ markov.leonid/T3/Commands.cpp
@@ -10,12 +10,12 @@ namespace mrkv
 {
   int isArgANumber(const std::string& str)
   {
-	char* end;
-	int num = strtol(str.c_str(), &end, 10);
+    char* end;
+    int num = strtol(str.c_str(), &end, 10);
     if (*end != 0)
-	{
+    {
       return -1;
-	}
+    }
     return num;
   }
 
@@ -24,36 +24,32 @@ namespace mrkv
     int num = isArgANumber(arg);
 
     if (num == -1)
-	{
+    {
       if (arg == "EVEN")
-	  {
-        return std::accumulate(figures.begin(), figures.end(), 0.0,
-          std::bind(areaAccumulator, _1, _2, 2, 0));
-	  }
+      {
+        return std::accumulate(figures.begin(), figures.end(), 0.0, std::bind(areaAccumulator, _1, _2, 2, 0));
+      }
       else if (arg == "ODD")
-	  {
-        return std::accumulate(figures.begin(), figures.end(), 0.0,
-          std::bind(areaAccumulator, _1, _2, 2, 1));
-	  }
+      {
+        return std::accumulate(figures.begin(), figures.end(), 0.0, std::bind(areaAccumulator, _1, _2, 2, 1));
+      }
       else if (arg == "MEAN" && figures.size() >= 1)
-	  {
-        return std::accumulate(figures.begin(), figures.end(), 0.0,
-          std::bind(areaAccumulator, _1, _2, 2, -1)) / figures.size();
-	  }
+      {
+        return std::accumulate(figures.begin(), figures.end(), 0.0, std::bind(areaAccumulator, _1, _2, 2, -1)) / figures.size();
+      }
       else
-	  {
+      {
         throw std::invalid_argument("<INVALID COMMAND>");
-	  }
-	}
+      }
+    }
     else if (num > 2)
-	{
-	  return std::accumulate(figures.begin(), figures.end(), 0.0,
-		std::bind(areaAccumulator, _1, _2, num, 0));
-	}
+    {
+      return std::accumulate(figures.begin(), figures.end(), 0.0, std::bind(areaAccumulator, _1, _2, num, 0));
+    }
     else
-	{
+    {
       throw std::invalid_argument("<INVALID COMMAND>");
-	}
+    }
   }
 
   double maxArea(std::vector<Polygon>& figures)
@@ -78,31 +74,31 @@ namespace mrkv
 
   int count(std::vector<Polygon>& figures, std::string& arg)
   {
-	int num = isArgANumber(arg);
+    int num = isArgANumber(arg);
 
     if (num == -1)
-	{
+    {
       if (arg == "EVEN")
-	  {
+      {
         return std::count_if(figures.begin(), figures.end(), std::bind(count—ondition, _1, 2, 0));
-	  }
+      }
       else if (arg == "ODD")
-	  {
+      {
         return std::count_if(figures.begin(), figures.end(), std::bind(count—ondition, _1, 2, 1));
-	  }
+      }
       else
-	  {
+      {
         throw std::invalid_argument("<INVALID COMMAND>");
-	  }
-	}
+      }
+    }
     else if (num > 2)
-	{
+    {
       return std::count_if(figures.begin(), figures.end(), std::bind(count—ondition, _1, num, 0));
-	}
+    }
     else
-	{
+    {
       throw std::invalid_argument("<INVALID COMMAND>");
-	}
+    }
   }
 
   int echo(std::vector<Polygon>& figures, Polygon& forAdd)
@@ -111,28 +107,28 @@ namespace mrkv
     if (numOfInsertions == 0)
     {
       return 0;
-	}
-	
+    }
+
     std::vector<Polygon> tmp = figures;
     bool flag = false;
     int offset = -1, counter = 0;
     auto lambda = [&forAdd, &tmp, &flag, &offset, &counter](Polygon& poly)
-    {
-      if (flag == true)
       {
-        auto it = std::find(tmp.begin() + offset, tmp.end(), forAdd);
-        if (it != tmp.end())
+        if (flag == true)
         {
-          tmp.insert(it, forAdd);
+          auto it = std::find(tmp.begin() + offset, tmp.end(), forAdd);
+          if (it != tmp.end())
+          {
+            tmp.insert(it, forAdd);
+          }
+          ++offset;
+          ++counter;
+          flag = false;
         }
+        flag = (poly == forAdd) ? true : false;
         ++offset;
-        ++counter;
-        flag = false;
-      }
-      flag = (poly == forAdd) ? true : false;
-      ++offset;
-      return false;
-    };
+        return false;
+      };
 
     std::copy_if(figures.begin(), figures.end(), tmp.begin(), lambda);
 
@@ -150,12 +146,12 @@ namespace mrkv
   {
     if ((getLeftB(input) >= getLeftFiguresB(figures) && getDownB(input) >= getDownFiguresB(figures))
       && (getRightB(input) <= getRightFiguresB(figures) && getUpB(input) <= getUpFiguresB(figures)))
-	{
+    {
       return "<TRUE>";
-	}
+    }
     else
-	{
+    {
       return "<FALSE>";
-	}
+    }
   }
 }
