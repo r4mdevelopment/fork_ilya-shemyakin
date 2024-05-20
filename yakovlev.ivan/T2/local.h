b04cec422088aa0f239669b56d66453241fcb3b1
User <Ivanlbt05@yandex.ru>
2024-05-21 00:08:04 +0300

    load work to check

---
+++ yakovlev.ivan/T2/local.h
@@ -0,0 +1,14 @@
+#ifndef LOCAL_H 
+#define LOCAL_H 
+
+#include <iostream> 
+#include <sstream> 
+#include <string> 
+#include <iterator> 
+#include <vector> 
+#include <iomanip> 
+#include <algorithm> 
+#include <cmath> 
+#include <exception> 
+
+#endif
--- yakovlev.ivan/T2/main.cpp
+++ yakovlev.ivan/T2/main.cpp
@@ -1,2 +1,39 @@
+﻿#include "namespace.h"
+
+using yakovlev::DataStruct;
+
 int main()
-{}
+{
+  try
+  {
+    std::string input = "";
+    std::vector< DataStruct > data;
+
+    while (std::getline(std::cin, input))
+    {
+      std::istringstream iss(input);
+      DataStruct temp;
+      if (iss >> temp)
+      {
+        data.push_back(temp);
+      }
+    }
+
+    // Сортировка вектора
+    std::sort(std::begin(data), std::end(data), yakovlev::compareDataStruct);
+
+    // Вывод отсортированных данных
+    std::copy(
+      std::begin(data),
+      std::end(data),
+      std::ostream_iterator< DataStruct >(std::cout, "\n")
+    );
+  }
+  catch (std::exception& ex)
+  {
+    std::cerr << ex.what();
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
