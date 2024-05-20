51b2b87e11f044a4ecf05a381998e960235eee9c
hismatulin.vadim <vadim-chita@yandex.ru>
2024-05-20 23:42:15 +0300

    Исправил замечания по лабе (1)

---
+++ hismatulin.vadim/T2/datastruct_hismatulin.h
@@ -0,0 +1,70 @@
+#ifndef NAMESPACE_H
+#define NAMESPACE_H
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <iterator>
+#include <vector>
+#include <iomanip>
+#include <complex>
+#include <algorithm>
+#include <cmath>
+#include <exception>
+
+namespace hismatulin
+{
+  struct DataStruct
+  {
+    unsigned long long key1;
+    std::complex<double> key2;
+    std::string key3;
+  };
+  struct DelimiterIO
+  {
+    char exp;
+  };
+  struct CMPDouble_ST
+  {
+    std::complex<double>& ref;
+  };
+  struct ULL_ST
+  {
+    unsigned long long& ref;
+  };
+  struct Double_ST
+  {
+    double& num;
+  };
+  struct String_ST
+  {
+    std::string& ref;
+  };
+  struct Label_ST
+  {
+    std::string exp;
+  };
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios< char >& s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char >& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, CMPDouble_ST&& dest);
+  std::istream& operator>>(std::istream& in, ULL_ST&& dest);
+  std::istream& operator>>(std::istream& in, String_ST&& dest);
+  std::istream& operator>>(std::istream& in, Label_ST&& dest);
+  std::istream& operator>>(std::istream& in, DataStruct& dest);
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
+
+  bool compareDataStruct(const DataStruct& ds_first, const DataStruct& ds_second);
+}
+#endif
+
+
--- hismatulin.vadim/T2/main.cpp
+++ hismatulin.vadim/T2/main.cpp
@@ -1,30 +1,34 @@
-#include "namespace.h"
+#include "datastruct_hismatulin.h"
 
 using hismatulin::DataStruct;
 
 int main()
 {
+  // ULL LIT CMP LSP
   try
   {
     std::string input = "";
     std::vector<DataStruct> data;
 
-    while (std::getline(std::cin, input))
+    while (!std::cin.eof())
     {
-      std::istringstream iss(input);
-      DataStruct temp;
-      if (iss >> temp)
+      std::copy(
+        std::istream_iterator<DataStruct>(std::cin), // итератор начала потока
+        std::istream_iterator<DataStruct>(), // итератор конца потока (без параметров т.к. обозначается конец потока)
+        std::back_inserter(data) // создает итератор, который добавляет значение в конец контейнера с помощью push_back
+      );
+      if (std::cin.fail() && !std::cin.eof()) // если произошла ошибка ввода или конец файла
       {
-        data.push_back(temp);
+        std::cin.clear(); // чистим ошибки, из-за некорректного ввода
       }
     }
-
+    
     std::sort(std::begin(data), std::end(data), hismatulin::compareDataStruct);
 
     std::copy(
       std::begin(data),
       std::end(data),
-      std::ostream_iterator< DataStruct >(std::cout, "\n")
+      std::ostream_iterator<DataStruct>(std::cout, "\n")
     );
   }
   catch (std::exception& ex)
