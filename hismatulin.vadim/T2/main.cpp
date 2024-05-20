62df3950c6d455e1b2513e5de95d403d8352e42b
hismatulin.vadim <vadim-chita@yandex.ru>
2024-05-20 23:45:15 +0300

    Исправил замечания по лабе (2)

--- hismatulin.vadim/T2/main.cpp
+++ hismatulin.vadim/T2/main.cpp
@@ -4,27 +4,23 @@ using hismatulin::DataStruct;
 
 int main()
 {
-  // ULL LIT CMP LSP
   try
   {
     std::string input = "";
     std::vector<DataStruct> data;
-
     while (!std::cin.eof())
     {
       std::copy(
-        std::istream_iterator<DataStruct>(std::cin), // итератор начала потока
-        std::istream_iterator<DataStruct>(), // итератор конца потока (без параметров т.к. обозначается конец потока)
-        std::back_inserter(data) // создает итератор, который добавляет значение в конец контейнера с помощью push_back
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
       );
-      if (std::cin.fail() && !std::cin.eof()) // если произошла ошибка ввода или конец файла
+      if (std::cin.fail() && !std::cin.eof())
       {
-        std::cin.clear(); // чистим ошибки, из-за некорректного ввода
+        std::cin.clear();
       }
     }
-    
     std::sort(std::begin(data), std::end(data), hismatulin::compareDataStruct);
-
     std::copy(
       std::begin(data),
       std::end(data),
@@ -36,6 +32,5 @@ int main()
     std::cerr << ex.what();
     return EXIT_FAILURE;
   }
-
   return EXIT_SUCCESS;
 }
